32ff22fbea0ebf3046d4878cf3113f7245aa7d40
Haris <muhamad_haris@hotmail.com>
2020-10-17 20:38:21 +0800

    Rename answer model to choice

--- lib/models/answer.dart
+++
@@ -1,7 +0,0 @@
-class Answer {
-  final String text;
-  final bool isCorrect;
-  final int id;
-
-  Answer({this.text, this.isCorrect, this.id});
-}
---
+++ lib/models/choice.dart
@@ -0,0 +1,7 @@
+class Choice {
+  final String text;
+  final bool isAnswer;
+  final int id;
+
+  Choice({this.text, this.isAnswer, this.id});
+}
